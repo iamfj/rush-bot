c7426f7174081e48a075540deaff9d8f1bd4057b
Fabian Jocks <dev@jocks.io>
2021-11-30 17:07:08 +0100

    Added dotenv library

--- src/rush.ts
+++ src/rush.ts
@@ -1,5 +1,8 @@
 import { Server } from 'socket.io';
 import { ServerHandler } from './handler/serverHandler';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const server = new Server(3000, {});
 const serverHandler = new ServerHandler(server);
