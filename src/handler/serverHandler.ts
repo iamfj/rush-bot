a591fefd480eb7d53134c90d72684409b38649af
Fabian Jocks <dev@jocks.io>
2021-11-30 19:18:25 +0100

    Added logger library

--- src/handler/serverHandler.ts
+++ src/handler/serverHandler.ts
@@ -1,11 +1,14 @@
 import { Server, Socket } from 'socket.io';
+import { Logger } from 'tslog';
 
 export class ServerHandler {
-  public constructor(private readonly server: Server) {
+  public constructor(private readonly server: Server, private readonly logger: Logger) {
     this.server.on('connection', this.onConnect);
+
+    this.logger.info(`Server started`)
   }
 
   private onConnect(socket: Socket): void {
-    console.log(`On connection ${socket}`);
+    this.logger.info(`On connection ${socket}`);
   }
 }
--- src/rush.ts
+++ src/rush.ts
@@ -1,8 +1,10 @@
 import { Server } from 'socket.io';
 import { ServerHandler } from './handler/serverHandler';
+import { Logger } from 'tslog';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
+const logger: Logger = new Logger();
 const server = new Server(3000, {});
-const serverHandler = new ServerHandler(server);
+const serverHandler = new ServerHandler(server, logger);
