38ff339a58add38da2f3c1d86ee5abd0b23d8fe0
Fabian Jocks <dev@jocks.io>
2021-12-02 23:22:25 +0100

    Fixed logger configuration

--- src/rush.ts
+++ src/rush.ts
@@ -11,8 +11,15 @@ dotenv.config();
 const rushPort: number = parseInt(process.env.RUSH_PORT ?? '3000');
 const strategyDir: string = path.join(process.cwd(), 'strategies');
 
-const logger: Logger = new Logger();
+// Configure logger instance
+const logger: Logger = new Logger({
+  displayFilePath: 'hidden',
+  displayFunctionName: false
+});
+
+// Register container objects
 container.register<Logger>(Logger, {useValue: logger});
 
+// Define main components
 const strategyManager = container.resolve(StrategyManager);
 strategyManager.load(strategyDir);
