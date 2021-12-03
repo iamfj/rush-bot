527cafe64bf4fe717bcbd858b2c50970103d976c
Fabian Jocks <dev@jocks.io>
2021-12-03 15:42:54 +0100

    Added usage of namedResolver class for strategies

--- src/resolver/strategyResolver.ts
+++ src/resolver/strategyResolver.ts
@@ -1,25 +1,10 @@
 import { singleton } from 'tsyringe';
 import { IStrategy } from '../interface/strategyInterface';
+import { NamedResolver } from './namedResolver';
 
 @singleton()
-export class StrategyResolver {
-  private strategies: { [key: string]: IStrategy } = {};
-
-  public register(strategy: IStrategy): void {
-    if (this.strategies[strategy.name] !== undefined) {
-      throw new Error(`An strategy with the name ${strategy.name} is already registered`);
-    }
-
-    this.strategies[strategy.name] = strategy;
-  }
-
-  public resolve(name: string): IStrategy {
-    const strategy: IStrategy | undefined = this.strategies[name];
-
-    if (strategy === undefined) {
-      throw new Error(`An strategy with the name ${name} is not registered`);
-    }
-
-    return strategy;
+export class StrategyResolver extends NamedResolver<IStrategy> {
+  public constructor() {
+    super('strategy');
   }
 }
