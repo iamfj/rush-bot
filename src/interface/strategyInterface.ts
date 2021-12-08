d796a0ff3862e7bf6d0412ad18c837c3f7ac41c7
Fabian Jocks <dev@jocks.io>
2021-12-08 17:16:24 +0100

    Added labeled interface to strategy

--- src/interface/strategyInterface.ts
+++ src/interface/strategyInterface.ts
@@ -1,10 +1,10 @@
 import { Options, StrategyOptions } from '../types/strategyTypes';
+import { ILabeled } from './labeledInterface';
 import { INamed } from './namedInterface';
 
-export interface IStrategy extends INamed {
-  label: string;
-  version: number;
-  options: StrategyOptions;
+export interface IStrategy extends INamed, ILabeled {
+  readonly version: number;
+  readonly options: StrategyOptions;
   onStart(options: Options): void;
   onTick(): void;
 }
